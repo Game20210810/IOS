598ed5bc1b41fed425cddc1467bd1da5da231d25
Game20210810 <88695500+Game20210810@users.noreply.github.com>
2025-01-13 10:02:29 +0800

    Create platform.js
    iOS

---
+++ platform.js
@@ -0,0 +1,7 @@
+[
+  "p/9ajKuEdssvMgTSKRu99fGTZLqivn6gmAT6Yo7IQzMC1Yd8ivv2rur4RDRnsnmq",
+  "vSwvVoDAf+lNTSsJWe+vd97LfL24XlzHaURH8rqqgtJ/MTZ6yRwcpVaabCqnMXum",
+  "wcb3lOpUASP56ah3lyCcZkBOQdzRoD0LDoqcNHCHSIGF23jpnvoxJxnBNVfN0Huz",
+  "HMBe8ZXMKiluwHu8i1c939u+hnEoE51vGpjT7cVOoWP/j81cg7Qhgq9h/FVein5w",
+  "ky10TIi6eOCNzqiu9EN9wYKtlte9jdjiUc8epmRQho5aDxrSFPm1yAhFm5X2na/1"
+]
